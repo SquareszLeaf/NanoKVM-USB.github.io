a3cdff8ac94f4586233d9d2fc7b21943098ab142
Squaresz <171099505+SquareszLeaf@users.noreply.github.com>
2025-11-17 20:00:25 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "nanokvm-usb-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./assets/index-FqlCgX2v.js",
+  "./assets/index-BpYGjG69.css",
+  "./sipeed.ico"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys.map(key => {
+            if (key !== CACHE_NAME) {
+              return caches.delete(key);
+            }
+            return undefined;
+          })
+        )
+      )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then(response => {
+        const responseClone = response.clone();
+        if (event.request.method === "GET" && response.ok) {
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        }
+        return response;
+      });
+    })
+  );
+});
